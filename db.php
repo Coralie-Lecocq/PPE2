7424e96cb3b20e665463c551e45fc7d2edce7ccd
Coralie <coralie.lecocq.62@gmail.com>
2019-04-11 16:56:03 +0200

    add api

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 /* DEBUT DE L'INITIALISATION DE LA CONNEXION DB */
     try{
-        $bdd = new PDO('mysql:host=localhost;dbname=ppe2', "root", "");
+        $bdd = new PDO('mysql:host=xd3r5.myd.infomaniak.com;dbname=xd3r5_coralie', "xd3r5_coralie", "1GVNINLyRSQq");
         $bdd->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
     }
     catch(PDOException $e){
