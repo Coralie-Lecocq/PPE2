00104ef409808ec9d5917be48f72a6a9a0d8225f
Coralie <coralie.lecocq.62@gmail.com>
2019-04-19 12:01:42 +0200

    fix

--- login.php
+++ login.php
@@ -5,16 +5,15 @@
     /* DEBUT VERIFICATION DE LA RECEPTION DE FORMULAIRE */
     if (isset($_POST['user']) && isset($_POST['passwd'])){      // cas ou on reçoit un formulaire
         $login = addslashes($_POST['user']);
-        $passwd = md5($_POST['passwd']);
+        $passwd = $_POST['passwd'];
         
         $reqUsr = 'SELECT * FROM clients WHERE email LIKE "' .$login.'"';
-        echo $reqUsr;
         if($usr = $bdd->query($reqUsr)){
             if($u = $usr->fetch()){
                 if($u->pwd == $passwd){
                     $_SESSION['nom'] = $u->nom;
                     $_SESSION['prenom'] = $u->prenom;
-                  
+                    echo "connecté(e)";
                 }else{
                     echo "erreur dans le mot de passe";
                 }   
