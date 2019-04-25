862eecf93e76b9ff61a6f64026e3b2365bfac238
Coralie <coralie.lecocq.62@gmail.com>
2019-04-25 16:38:18 +0200

    add panier

---
+++ addpanier.php
@@ -0,0 +1,19 @@
+<?php
+    include('db.php');
+    $panier = new Panier();
+
+    if(isset($_GET['id'])) {
+        $product = $bdd->query('SELECT idconsommable FROM consommables WHERE idconsommable='. $_GET['id']);
+        if(empty($product)) {
+            die('Ce produit n\'existe pas');
+        }
+        $panier->add($product[0]->idconsommable);
+        ?>
+        <script>
+            document.location.href = "index.php";
+        </script>
+      <?php
+    } else {
+        die('pas de produit');
+    }
+?>
